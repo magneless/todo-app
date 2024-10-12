6b2020aad5fdb2ab0a13edc3e9dbbed4246b834b
magomed <mrnavruzov@edu.hse.ru>
2024-10-13 00:05:51 +0300

    Added authentication

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,12 +3,23 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/magneless/todo-app"
 	"github.com/magneless/todo-app/pkg/repository"
 )
 
-const salt = "akaka101010jdjddj"
+const (
+	salt = "akaka101010jdjddj"
+	signingKey = "adafasfasf12"
+	tokenTTL = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -18,9 +29,26 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 	return &AuthService{repo : repo}
 }
 
-func (s* AuthService) CreateUser(user todo.User) (int, error) {
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = s.generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt: time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func (s *AuthService) generatePasswordHash(password string) string {
