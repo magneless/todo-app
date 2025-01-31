09679aebfb0fda9919b74e779d575c8914e7e492
magomed <mrnavruzov@edu.hse.ru>
2025-01-31 20:59:07 +0300

    Added log/slog implementation

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -1,36 +1,68 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/magneless/todo-app/internal/config"
 	"github.com/magneless/todo-app/internal/http-server/router"
-	reposiory "github.com/magneless/todo-app/internal/repository"
+	"github.com/magneless/todo-app/internal/lib/logger/sl"
+	repository "github.com/magneless/todo-app/internal/repository"
 	"github.com/magneless/todo-app/internal/storage/postgre"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting todo-app", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
 	storage, err := postgre.New(cfg.Storage)
 	if err != nil {
-		log.Fatal(err)
+		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
 
-	reposiory.New(storage)
+	repo := repository.New(storage)
 
 	srv := &http.Server{
 		Addr:         cfg.Address,
-		Handler:      router.New(),
+		Handler:      router.New(log, repo),
 		ReadTimeout:  cfg.Timeout,
 		WriteTimeout: cfg.Timeout,
 		IdleTimeout:  cfg.IdleTimeout,
 	}
-	
+
 	if err := srv.ListenAndServe(); err != nil {
 
 	}
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
